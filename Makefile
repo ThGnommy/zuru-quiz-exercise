97a91ae587f4a95c00f25c62c9ec0833f70d6122
ThGnommy <th.brandoli@gmail.com>
2023-09-19 12:40:48 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	g++ -Wall -std=c++17 -o quiz main.cpp;
+
+run:
+	./quiz
+
+clean:
+	rm quiz
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hi quiz." << std::endl;
+  return 0;
+}
